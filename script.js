1cc869ece38c62b6bb15a704bf8da3b353d83e59
BSV17 <seregars17@gmail.com>
2022-04-21 17:33:46 +0300

    Update homework 8

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const obj = {
     age: 30
 };
 
-const arr = [1, 2, 3, 4, 5];
+const arr1 = [1, 2, 3, 4, 5];
+
+const arr2 = [1, 2, [3, 3.1, 3.2], 4, 5];
 
 const str = "Hello world";
 
@@ -14,7 +16,15 @@ function deepCopy(data) {
     if (data === null) return null;
     if (Array.isArray(data)) {
         copiedData = [];
-        copiedData = data.slice(0);
+        for (let key in data) {
+            if (data.hasOwnProperty(key)) {
+                if (Array.isArray(data[key])) {
+                    copiedData[key] = deepCopy(data[key])
+                } else {
+                    copiedData[key] = data[key]
+                }
+            }
+        }
     } else if (typeof data === "object" && !Array.isArray(data)) {
         copiedData = {};
         for (let key in data) {
@@ -29,6 +39,7 @@ function deepCopy(data) {
 }
 
 console.log(deepCopy(obj));
-console.log(deepCopy(arr));
+console.log(deepCopy(arr1));
+console.log(deepCopy(arr2));
 console.log(deepCopy(str));
 console.log(deepCopy(num));
